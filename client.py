47253bc87b6c02d5927966456fda4c0228eaf24b
matheusfelipe190904 <140838689+matheusfelipe190904@users.noreply.github.com>
2026-03-23 15:24:02 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,9 +1,28 @@
-from socket  import *
-from constCS import * #-
+from socket import *
+from constCS import *
 
 s = socket(AF_INET, SOCK_STREAM)
-s.connect((HOST, PORT)) # connect to server (block until accepted)
-s.send(str.encode('Hello, world'))  # send some data
-data = s.recv(1024)     # receive the response
-print (bytes.decode(data))            # print the result
-s.close()               # close the connection
+s.connect((HOST, PORT))
+
+print("Conectado ao servidor!")
+
+while True:
+    print("\nOperações disponíveis:")
+    print("add, sub, mul, div, pow")
+    print("Digite 'sair' para encerrar")
+
+    op = input("Operação: ")
+
+    if op == "sair":
+        break
+
+    v1 = input("Valor 1: ")
+    v2 = input("Valor 2: ")
+
+    msg = f"{op};{v1};{v2}"
+    s.send(msg.encode())
+
+    data = s.recv(1024)
+    print("Resultado:", data.decode())
+
+s.close()
